2d80a464a3b86854ee4da4d81dd93bb0d752071b
Chun Yang <canadachunyang@gmail.com>
2020-05-27 21:05:55 -0400

    set up redux

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -56,8 +56,8 @@ class SignUp extends React.Component {
         const { displayName, email, password, confirmPassword } = this.state
         return(
             <div className='sign-up'>
-                <h2 className='title'>I do not have an account</h2>
-                <span>Sign up with your email and password</span>
+                <h2>I do not have an account</h2>
+                <span className='title'>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit} >
                     <FormInput
                         type='text'
