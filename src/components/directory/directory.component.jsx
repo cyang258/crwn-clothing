1ea8251c365fdc85ac472072abffba975f783375
Chun Yang <canadachunyang@gmail.com>
2020-06-02 18:35:29 -0400

    move Directory State and Collection State into Redux and add CollectionOverview component to shop page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './directory.styles.scss';
+
 import MenuItem from '../menu-item/menu-item.component';
+import { selectDirectorySections } from '../../redux/directory/directory.selector';
 
-import SECTIONS_DATA from '../../data/sections.data.js';
-
-class Directory extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            sections: SECTIONS_DATA
+const Directory = ({ sections }) => (
+    <div className='directory-menu'>
+        {
+            sections.map(({ id, ...otherSectionProps }) => (
+                <MenuItem key={id} {...otherSectionProps} />
+            ))
         }
-    }
+    </div>
+)
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({ id, ...otherSectionProps }) => (
-                        <MenuItem key={id} {...otherSectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
+const mapStateToProps = createStructuredSelector ({
+    sections: selectDirectorySections
+})
 
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import SHOP_DATA from '../../data/shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-export default class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+);
 
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+export default ShopPage;
